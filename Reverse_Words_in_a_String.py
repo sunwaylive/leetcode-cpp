9e78c9d5db0a96656bcb435d709b506cccf6935f
Sunway <sunwayliving@gmail.com>
2014-04-24 21:24:54 +0800

    finish Reverse Words in a String

--- Reverse_Words_in_a_String.py
+++ Reverse_Words_in_a_String.py
@@ -1,10 +1,35 @@
 import re
 
 #main function
-string = input("input the string to inverse: ")
-string.strip()
-space_re = re.compile('[ ]+')
-string = space_re.sub(' ', string)
-word_list = string.split(" ")
-result = ' '.join(word_list)
+#string = input("input the string to inverse: ")
+#string.strip()
+#space_re = re.compile('[ ]+')
+#string = space_re.sub(' ', string)
+#word_list = string.split(" ")
+#result = ' '.join(word_list)
+#print(result)
+
+class Solution:
+    def reverseWords(self, s):
+        word_list = []
+        word = ''
+        for c in s:
+            if c == ' ':
+                if word != '':
+                    word_list.append(word)
+                    word = ''
+                else:
+                    continue
+            else:
+                word += c
+        if word != '':
+            word_list.append(word)
+        word_list.reverse()
+        result = ' '.join(word_list)
+        return result
+
+#main function
+sln = Solution()
+string = "   the sky is    blue  "
+result = sln.reverseWords(string);
 print(result)
