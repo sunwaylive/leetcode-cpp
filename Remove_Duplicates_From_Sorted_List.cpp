f1471078a046f3adc224ca7caa075c39297d5078
sunwei <sunwayliving@gmail.com>
2014-06-17 17:30:17 +0800

    add O(n) solution

--- Remove_Duplicates_From_Sorted_List.cpp
+++ Remove_Duplicates_From_Sorted_List.cpp
@@ -7,6 +7,7 @@ struct ListNode{
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+//method 1: works for all link lists
 ListNode* deleteDuplicates(ListNode *head)
 {
     ListNode *p, *q, *pre;
@@ -27,13 +28,56 @@ ListNode* deleteDuplicates(ListNode *head)
     return head;
 }
 
+//method 2: only works for sorted link list
+ListNode* deleteDuplicates2(ListNode *head)
+{
+    if(head == NULL) return head;
+
+    ListNode *prev, *cur;
+    for(prev = head, cur = prev->next; cur != NULL;){
+        if(prev->val == cur->val){
+            prev->next = cur->next;
+            ListNode *tmp = cur;
+            cur = cur->next;
+            delete tmp;
+        }else{
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return head;
+}
+
+//method3: only works for sorted link list
+void recurDelete(ListNode *prev, ListNode *cur)
+{
+    if(cur == NULL) return;
+
+    if(prev->val == cur->val){
+        prev->next = cur->next;
+        delete cur;
+        recurDelete(prev, prev->next);
+    }else{ recurDelete(prev->next, cur->next); }
+}
+
+ListNode* deleteDuplicatesRecur(ListNode *head)
+{
+    if(head == NULL) return head;
+
+    ListNode dummy(head->val - 1);//different with head->val is ok
+    dummy.next = head;
+
+    recurDelete(&dummy, head);
+    return dummy.next;
+}
+
 ListNode* generateList(int size)
 {
     ListNode *head = (ListNode*)malloc(sizeof(ListNode));
     ListNode *cur = head;
     for(int i = 0; i < size; i++){
     ListNode *p = (ListNode*)malloc(sizeof(ListNode));
-    // srand((unsigned)time(NULL));
+    srand((unsigned)time(NULL));
     p->val = rand() % 10;
     p->next = NULL;
     cur->next = p;
@@ -57,7 +101,7 @@ int main()
     ListNode *l2 = generateList(10);
     print_list(l2);
 
-    ListNode *re = deleteDuplicates(l2);
+    ListNode *re = deleteDuplicates2(l2);
     print_list(re);
     return 0;
 }
