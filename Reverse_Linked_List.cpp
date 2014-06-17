8d586d71de7b8701eadadb38554dd94c38416afa
sunwei <sunwayliving@gmail.com>
2014-06-17 14:27:50 +0800

    my first solution

---
+++ Reverse_Linked_List.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void print_list(ListNode *head);
+
+ListNode* findPos(ListNode *head, int n)
+{
+    ListNode *pos = head;
+    while(n-- > 1 && pos != NULL){
+        pos = pos->next;
+    }
+    return pos;
+}
+
+ListNode *reverseList(ListNode *head)
+{
+    if(head == NULL || head->next == NULL) return head;
+
+    ListNode *p, *q, *r;
+    for(p = head, q = p->next, r = q->next; q != NULL;){
+        q->next = p;
+        p = q;
+        q = r;
+        r = (r == NULL) ? NULL : r->next;
+    }
+    head->next = NULL;
+    return p;
+}
+
+ListNode* reverseBetween(ListNode *head, int m, int n)
+{
+    ListNode dummy(-1);
+    dummy.next = head;
+    ListNode *dummy_head = &dummy;
+
+    ListNode *pre_start = findPos(dummy_head, m);
+    ListNode *pre_end = findPos(dummy_head, n);
+    ListNode *start = pre_start->next;
+    ListNode *end = pre_end->next;
+
+    ListNode *tmp = end->next;
+    end->next = NULL;
+    ListNode *new_start = reverseList(start);
+    pre_start->next = new_start;
+    start->next = tmp;
+    return dummy.next;
+}
+
+ListNode* generateList(int size)
+{
+    ListNode *head = (ListNode*)malloc(sizeof(ListNode));
+    ListNode *cur = head;
+    for(int i = 0; i < size; i++){
+    ListNode *p = (ListNode*)malloc(sizeof(ListNode));
+    // srand((unsigned)time(NULL));
+    p->val = rand() % 10;
+    p->next = NULL;
+    cur->next = p;
+    cur = cur->next;
+    }
+    return head->next;
+}
+
+void print_list(ListNode *l)
+{
+    ListNode *tmp = l;
+    while(tmp != NULL){
+        cout<< tmp->val <<" ";
+        tmp = tmp->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    ListNode *l2 = generateList(5);
+    print_list(l2);
+
+    ListNode *re = reverseBetween(l2, 1, 4);
+    print_list(re);
+    return 0;
+}
